d257de3c25cb5dc3e801d9a1f29d388f1b512a60
Siddarta Weissbock <siddarta.weis@gmail.com>
2021-11-19 12:36:25 -0300

    Update urls.py

--- containers/urls.py
+++ containers/urls.py
@@ -2,9 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import *
 
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
